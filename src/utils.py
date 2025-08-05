e1d03df67d847999b79b416ff618241d12c7988c
villeneve <villeneveoliveira@gmail.com>
2025-08-05 19:37:53 -0300

    update all files last

--- src/utils.py
+++ src/utils.py
@@ -13,24 +13,24 @@ def r1_regularization(discriminator, batch):
     return norm
 
 @tf.function
-def train_step(gan,data,opt,batch_size,epoch):
+def train_step(gan,batch,opt,batch_size,epoch):
     g_loss = 0.
     d_loss = 0.
-    for batch in data:
-        latent_z = tf.random.normal((batch_size,128))
-        with tf.GradientTape() as g_tape, tf.GradientTape() as d_tape:
-            fake_imgs = gan[0](latent_z, trainable=True)
-            true_logis = gan[1](batch,trainable=True)
-            fake_logits = gan[1](fake_imgs, trainable=True)
-            g_loss = keras.losses.binary_crossentropy(tf.ones_like(fake_logits), fake_logits)# - 1e-4*tf.reduce_sum(tf.math.reduce_std(fake_imgs,axis=0))
-            d_loss = keras.losses.binary_crossentropy(tf.ones_like(true_logis),true_logis)+keras.losses.binary_crossentropy(tf.zeros_like(fake_logits),fake_logits)
-            if epoch%8 == 0:
-                d_loss += 5*r1_regularization(gan[1],batch)
+    latent_z = tf.random.normal((batch_size,128))
+    with tf.GradientTape() as g_tape, tf.GradientTape() as d_tape:
+        fake_imgs = gan[0](latent_z, trainable=True)
+        true_logis = gan[1](batch,trainable=True)
+        fake_logits = gan[1](fake_imgs, trainable=True)
+        g_loss = keras.losses.binary_crossentropy(tf.ones_like(fake_logits), fake_logits)# - 1e-4*tf.reduce_sum(tf.math.reduce_std(fake_imgs,axis=0))
+        d_loss = keras.losses.binary_crossentropy(tf.ones_like(true_logis),true_logis)+keras.losses.binary_crossentropy(tf.zeros_like(fake_logits),fake_logits)
+        if epoch%10 == 0:
+            d_loss += r1_regularization(gan[1],batch)
 
+    g_grads = g_tape.gradient(g_loss,gan[0].trainable_variables)
+    opt[0].apply_gradients(zip(g_grads,gan[0].trainable_variables))
 
-        g_grads = g_tape.gradient(g_loss,gan[0].trainable_variables)
-        opt[0].apply_gradients(zip(g_grads,gan[0].trainable_variables))
-
-        d_grads = d_tape.gradient(d_loss,gan[1].trainable_variables)
-        opt[1].apply_gradients(zip(d_grads,gan[1].trainable_variables))
+    d_grads = d_tape.gradient(d_loss,gan[1].trainable_variables)
+    opt[1].apply_gradients(zip(d_grads,gan[1].trainable_variables))
+    
     return tf.reduce_mean(g_loss), tf.reduce_mean(d_loss)
+
--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import keras
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 from .src.model import *
 from .src.dataset_loader import *
@@ -14,8 +15,33 @@ discriminator = create_discriminator()
 gan = [generator, discriminator]
 
 # Dataset load
-cars = load_dataset(batch_size=64)
+batch_size = 64
+cars = load_dataset(batch_size)
 
 # Optimizers
 opt = create_opt()
 
+# Epochs
+epochs = 10000
+
+for epoch in range(epochs):
+    t = time.time()
+    for batch in cars:
+        g_loss, d_loss = train_step(gan,batch,opt,batch_size,epoch)
+    if epoch%10 == 0:
+        print(f'{epoch} - G = {g_loss:.4f}; D = {d_loss:.4f}; Time = {((time.time()-t)/60.):.2f}')
+
+    if epoch % 50 == 0:
+        n = 5
+        img = gan[0](tf.random.normal((n**2,128)),training=False)
+
+        fig, ax = plt.subplots(n,n,figsize=(7,7))
+        ax = ax.ravel()
+        for ii in range(n**2):
+            ax[ii].matshow(np.uint8(img[ii]*127.5+127.5))
+            ax[ii].set_axis_off()
+        plt.tight_layout(pad=0)
+        plt.savefig(f'fig_{epoch}.png')
+        plt.close()
+        del img
+        generator.save('style.keras')

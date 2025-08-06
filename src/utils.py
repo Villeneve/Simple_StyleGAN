fix gradient exploding

src/utils.py
@@ -5,10 +5,10 @@ import keras
 def r1_regularization(discriminator, batch):
     with tf.GradientTape() as tape:
         tape.watch(batch)
-        logits = discriminator(batch,training=True)
+        logits = discriminator(batch,training=False)
         logits = tf.reduce_sum(logits)
     grads = tape.gradient(logits,[batch])[0]
-    norm = tf.reduce_mean(tf.reduce_sum(tf.square(grads),axis=[1,2,3]))
+    norm = tf.reduce_mean(tf.reduce_sum(tf.square(grads),axis=[1,2,3])+1e-8)
     del grads
     return norm
 
@@ -21,9 +21,9 @@ def gradient_penalty(gan,batch):
     interpolation = fake_imgs + alpha*(batch-fake_imgs)
     with tf.GradientTape() as tape:
         tape.watch(interpolation)
-        y = gan[1](interpolation)
+        y = gan[1](interpolation, training=True)
     grads = tape.gradient(y,[interpolation])[0]
-    norm = tf.sqrt(tf.reduce_sum(tf.square(grads),axis=[1,2,3]))
+    norm = tf.sqrt(tf.reduce_sum(tf.square(grads),axis=[1,2,3])+1e-8)
     return tf.reduce_mean(tf.square(norm-1))
 
 @tf.function
@@ -38,8 +38,7 @@ def train_step(gan,batch,opt):
         true_logis = gan[1](batch,trainable=True)
         fake_logits = gan[1](fake_imgs, trainable=True)
         g_loss = -tf.reduce_mean(fake_logits)
-        d_loss = -(tf.reduce_mean(true_logis) - tf.reduce_mean(fake_logits))
-        d_loss += gradient_penalty(gan,batch)
+        d_loss = -(tf.reduce_mean(true_logis) - tf.reduce_mean(fake_logits)) + 10*gradient_penalty(gan,batch)
 
     g_grads = g_tape.gradient(g_loss,gan[0].trainable_variables)
     opt[0].apply_gradients(zip(g_grads,gan[0].trainable_variables))
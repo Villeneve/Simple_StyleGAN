36a2c39301a610d45b24f11dac67dddd3d5d7fa7
villeneve <villeneveoliveira@gmail.com>
2025-08-07 15:29:19 -0300

    fix: beta_1 from discriminator and generator

--- src/model.py
+++ src/model.py
@@ -123,4 +123,4 @@ def create_discriminator():
     return discriminator
 
 def create_opt():
-    return [keras.optimizers.Adam(1e-4,.55), keras.optimizers.Adam(2e-4,.9)]
+    return [keras.optimizers.Adam(1e-4,0.0,.99), keras.optimizers.Adam(2e-4,0.0,.99)]
